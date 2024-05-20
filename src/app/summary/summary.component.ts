215856ec92beb79299bb9003965a8834b8c0f9d0
Valeria Rodriguez <145387992+ValeriaNaige@users.noreply.github.com>
2024-05-19 19:55:25 -0600

    intento de summary

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
 import { Router } from '@angular/router';
+import { Run } from '../Interfaces/run';
+import { RunServiceService } from '../run-service.service';
 
 @Component({
   selector: 'app-summary',
@@ -9,7 +11,7 @@ import { Router } from '@angular/router';
 })
 export class SummaryComponent  implements OnInit {
 
-  constructor(public authService: AuthenticationService, public route:Router) { }
+  constructor(public authService: AuthenticationService, public route:Router, private servicio: RunServiceService) { }
 
   ngOnInit() {
     if(this.authService.activeUser==false){
@@ -17,4 +19,28 @@ export class SummaryComponent  implements OnInit {
     }
   }
 
+  carreras : Run[] = this.servicio.carreras;
+  x: any;
+
+  elegir(i: number): string{
+    this.x = this.carreras[i].distance;
+    if (this.x<=2){
+      console.log("int1");
+      return 'light';
+    }
+    else if (this.x>2 && this.x<=5){
+      console.log("int2");
+      return 'warning';
+    }
+    else if (this.x>5 && this.x<=10){
+      console.log("int3");
+      return 'tertiary';
+    }
+    else if (this.x>10){
+      console.log("int4");
+      return 'success';
+    }
+    console.log("noentró");
+    return 'warning';
+  } 
 }
